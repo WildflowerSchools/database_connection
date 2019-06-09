f511d001a6b23875e1f13fbb3459816ceaeb0221
Theodore Quinn <tcquinn@tcquinn.org>
2019-06-09 12:55:45 -0500

    Implement DataQueue class

--- database_connection/__init__.py
+++ database_connection/__init__.py
@@ -83,5 +83,81 @@ class DatabaseConnection:
             end_time (string): Return data with timestamps less than or equal to this value
             object_ids (list): Return data for these object IDs
             fields (list): Return data for these fields
+
+        Returns:
+            (list of dict): Datapoints from database which satisfy the criteria
+        """
+        raise NotImplementedError('Method must be implemented by derived class')
+
+    def to_data_queue(
+        self,
+        start_time = None,
+        end_time = None,
+        object_ids = None,
+        fields = None
+    ):
+        """
+        Create an iterable which returns datapoints from the database in time order.
+
+        Start time and end time must be ISO-format strings. If start time or end
+        time is specified and database does not have a designated timestamp
+        field, an exception will be generated.
+
+        If object IDs are specified and database does not have a designated
+        object ID field, an exception will be generated.
+
+        If fields are not specified, all fields are returned.
+
+        If database does not have a designated timestamp field, an exception
+        will be generated.
+
+        Parameters:
+            start_time (string): Return data with timestamps greater than or equal to this value
+            end_time (string): Return data with timestamps less than or equal to this value
+            object_ids (list): Return data for these object IDs
+            fields (list): Return data for these fields
+
+        Returns:
+            (DataQueue): Data queue which contains datapoints from database that satisfy the criteria
+        """
+        raise NotImplementedError('Method must be implemented by derived class')
+
+class DataQueue:
+    """
+    Class to define an iterable which returns datapoints in time order.
+
+    All methods must be implemented by derived classes.
+    """
+    def __init__(
+        self,
+        data,
+        timestamp_field_name
+        ):
+        """
+        Constructor for DataQueue
+
+        Data must be a simple list of dicts containing the datapoints (the
+        structure returned by DatabaseConnection.fetch_data()).
+
+        Every datapoint must contain a field with the specified timestamp field name.
+
+        Parameters:
+            data (list of dict): Name of the field containing the timestamp for each datapoint
+            object_id_field_name (string): Name of the field containing the object ID for each datapoint
+            other_field_names (list of string): Names of the remaining fields
+        """
+        raise NotImplementedError('Method must be implemented by derived class')
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        """
+        Fetch datapoint associated with the next timestamp.
+
+        Data will be fetched in time order.
+
+        Returns:
+            (dict): Data associated with the next timestamp
         """
         raise NotImplementedError('Method must be implemented by derived class')
