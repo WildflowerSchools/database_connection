cc7d1c82f3e93eb2aef9ee88488e13dcf3afb8e9
Theodore Quinn <tcquinn@tcquinn.org>
2019-06-10 16:56:55 -0500

    Remove __init__() function from parent DatabaseConnection class

--- database_connection/__init__.py
+++ database_connection/__init__.py
@@ -5,36 +5,6 @@ class DatabaseConnection:
 
     All methods must be implemented by derived classes.
     """
-    def __init__(
-        self,
-        timestamp_field_name = None,
-        object_id_field_name = None,
-        other_field_names = None):
-        """
-        Constructor for DatabaseConnection.
-
-        If timestamp field name is specified, then database is assumed to be a
-        time series database, every datapoint written to the database must
-        contain a field with this name, and this will enable various
-        time-related methods to access the data (e.g., fetching a time span,
-        creating an iterator that returns data points in time order).
-
-        If object ID field name is specified, then database is assumed to be
-        contain data associated with objects (e.g., measurement devices), every
-        datapoint written to the database must contain a field with this name,
-        and this will enable various object methods to access the data (e.g.,
-        fetching all data associated with a specific list of object IDs).
-
-        The final argument contains the names of the remaining fields. Any
-        subsequent data sent to the database that is not associated with one of
-        the field names in these three arguments will be silently rejected.
-
-        Parameters:
-            timestamp_field_name (string): Name of the field containing the timestamp for each datapoint
-            object_id_field_name (string): Name of the field containing the object ID for each datapoint
-            other_field_names (list of string): Names of the remaining fields
-        """
-        raise NotImplementedError('Method must be implemented by derived class')
 
     def write_data(
         self,
