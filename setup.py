ad440118b887bebcc6d68984f6b1b5028e98066f
Theodore Quinn <tcquinn@tcquinn.org>
2019-10-19 12:55:37 -0500

    Move version info to VERSION file

--- setup.py
+++ setup.py
@@ -2,6 +2,7 @@ import os
 from setuptools import setup, find_packages
 
 BASEDIR = os.path.dirname(os.path.abspath(__file__))
+VERSION = open(os.path.join(BASEDIR, 'VERSION')).read().strip()
 
 BASE_DEPENDENCIES = [
     'python-dateutil>=2.8.0'
@@ -13,7 +14,7 @@ os.chdir(os.path.normpath(BASEDIR))
 setup(
     name='wf-database-connection',
     packages=find_packages(),
-    version='0.1.0',
+    version=VERSION,
     include_package_data=True,
     description='A simple, generic database interface that can be adapted to many different use cases and implementations',
     long_description=open('README.md').read(),
